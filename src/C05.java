adc85d4d7374127d938496ad2687a17fa8855959
selmanbb <nurkasabali45@gmail.com>
2022-10-05 11:26:03 +0200

    2.deneme calismasi

--- src/C05.java
+++ src/C05.java
@@ -2,9 +2,9 @@ public class C05 {
     public static void main(String[] args) {
         System.out.println("C05");
 
-        System.out.println("deneme c05");
+        //System.out.println("deneme c05"); 2. calisma
 
-        System.out.println("deneme c05 ikinci degisiklik");
+        System.out.println("2.ci calismada degistirilen");
 
         System.out.println("deneme c05 ucuncu degisiklik");
 
