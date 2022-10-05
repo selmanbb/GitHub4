f1289ff0f5d1f6f0d4e6c34f697fe5cd771445cc
selmanbb <nurkasabali45@gmail.com>
2022-10-05 10:58:25 +0200

    deneme branch v2

--- src/C05.java
+++ src/C05.java
@@ -2,5 +2,7 @@ public class C05 {
     public static void main(String[] args) {
         System.out.println("C05");
         System.out.println("deneme c05");
+
+        System.out.println("deneme c05 ikinci degisiklik");
     }
 }
